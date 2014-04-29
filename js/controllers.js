0098deaebd0fd595506625054b89d3c3401f541b
Andrew <luzkovskyandrey@gmail.com>
2014-04-29 11:12:49 +0300

    modal

--- js/controllers.js
+++ js/controllers.js
@@ -88,7 +88,7 @@ angular.module('directory.controllers', [])
 //   });
        // debugger
        alert('test')
-           $scope.modal.show();
+          // $scope.modal.show();
         });
 
         // Stop the side bar from dragging when mousedown/tapdown on the map
@@ -100,25 +100,25 @@ angular.module('directory.controllers', [])
         $scope.map = map;
       }
 
-var modal = null;
+// var modal = null;
 
-      $ionicModal.fromTemplateUrl('templates/provider-detail.html', function(modal, a,b,c) {
+//       $ionicModal.fromTemplateUrl('templates/provider-detail.html', function(modal, a,b,c) {
         
-    $scope.modal = modal;
-   // modal.scope
-   // $scope.$hasHeader = false;
-  }, {
- //   scope: $scope,
-    animation: 'slide-in-up'
-  })
-  $scope.openModal = function() {
-    $scope.modal.show();
-  };
+//     $scope.modal = modal;
+//    // modal.scope
+//    // $scope.$hasHeader = false;
+//   }, {
+//  //   scope: $scope,
+//     animation: 'slide-in-up'
+//   })
+//   $scope.openModal = function() {
+//     $scope.modal.show();
+//   };
  
-  //Cleanup the modal when we're done with it!
-  $scope.$on('$destroy', function() {
-    $scope.modal.remove();
-  });
+//   //Cleanup the modal when we're done with it!
+//   $scope.$on('$destroy', function() {
+//     $scope.modal.remove();
+//   });
 
       //google.maps.event.addDomListener(window, 'load', initialize);
       
